c7dc34bce641a35fb1f6181ae5f5581bd40c16d7
songlijun <github@itjh.com.cn>
2020-07-01 09:54:43 +0800

    first commit

---
+++ app/src/main/java/com/example/zhanghengrong1801307020/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.zhanghengrong1801307020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
---
+++ app/src/main/java/com/example/zhanghengrong1801307020/SignNameActivity.java
@@ -0,0 +1,54 @@
+package com.example.zhanghengrong1801307020;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class SignNameActivity extends BaseActivity{
+
+    protected EditText signNameEdtx;
+    protected TextView saveTx,backTx;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_name_layout);
+        initView();
+        initData();
+    }
+
+    @Override
+    void initView() {
+        signNameEdtx = findViewById(R.id.sign_name_text);
+        saveTx = findViewById(R.id.save_tx);
+        backTx  = findViewById(R.id.back_tx);
+    }
+
+    @Override
+    void initData() {
+        saveTx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String signName = signNameEdtx.getText().toString().trim();
+                //跳转界面 传递个性签名
+                CacheUtil.setPermanent("signNmae",signName);
+                Intent  goMainTent  = new  Intent(SignNameActivity.this,MainActivity.class);
+                startActivity(goMainTent);
+                finish();
+            }
+        });
+        backTx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
